384c98799971a177d6736f1d8180a3466c7b0fda
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-28 04:53:18 +0000

    update files

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,49 +1,140 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.io.*;
+import java.lang.reflect.Method;
 
 public class PracticeProblemTest {
 
    @Test
-   public void testOutput()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
-
-     // action
-     PracticeProblem.q1();
-
-     // assertion
-     assertEquals("There once was a man from St. Ives.\n", bos.toString());
-
-     // undo the binding in System
-     System.setOut(originalOut);
+   @DisplayName("")
+   void goodStringTest1() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "bacdsfa");
+         assertEquals(result, 1);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
    }
 
    @Test
-   public void testInputandOutput()
-   {
-      String data = "Users Input";
-      System.setIn(new ByteArrayInputStream(data.getBytes()));
-      
-      PrintStream originalOut = System.out;
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos));
-
-      // action
-      PracticeProblem.q1();
-
-      // assertion
-      assertEquals("There once was a man from St. Ives.\n", bos.toString());
-
-      // undo the binding in System
-      System.setOut(originalOut);
+   @DisplayName("")
+   void goodStringTest2() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "abcda");
+         assertEquals(result, 0);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
    }
 
    @Test
-   public void testQ3()
-   {
-     
+   @DisplayName("")
+   void goodStringTest3() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "abcdefghiab");
+         assertEquals(result, 1);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
+   }
+
+   @Test
+   @DisplayName("")
+   void goodStringTest4() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "pqr");
+         assertEquals(result, -1);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
+   }
+
+   @Test
+   @DisplayName("")
+   void goodStringTest5() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "bacdefghipalop");
+         assertEquals(result, 4);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
+   }
+
+   @Test
+   @DisplayName("")
+   void goodStringTest6() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "abcdfedree");
+         assertEquals(result, 5);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
+   }
+
+   @Test
+   @DisplayName("")
+   void goodStringTest7() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { String.class,};
+         Method method = testClass.getDeclaredMethod("goodString", cArg);
+         // Enter code here
+         int result = (int)method.invoke(null, "abcdfghee");
+         assertEquals(result, 7);
+      }
+      catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } 
+      catch (Exception e) {
+         fail("Something weird happened");
+      }
    }
 }
